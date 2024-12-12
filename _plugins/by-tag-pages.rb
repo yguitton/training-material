2ce88091118f569000933a7eeb36fed718c8ac86
Helena Rasche <hexylena@galaxians.org>
2024-12-12 18:08:04 +0100

    restructure generators more sanely

--- _plugins/by-tag-pages.rb
+++ _plugins/by-tag-pages.rb
@@ -1,45 +1,47 @@
 require './_plugins/jekyll-topic-filter'
 
 module Jekyll
-  ##
-  # This class generates the GTN's author pags
-  class TagPageGenerator < Generator
-    safe true
-
+  module Generators
     ##
-    # This generates the author pages
-    # Params
-    # +site+:: The site object
-    def generate(site)
-      Jekyll.logger.info '[GTN/SyntheticTopics] Generating By-Tag Indexes'
-      Gtn::TopicFilter.list_all_tags(site).map do |tag|
-        site.data["by_tag_#{tag}"] = {
-          'name' => "by_tag_#{tag}",
-          'type' => 'use',
-          'title' => tag,
-          'summary' => "Tutorials covering #{tag}",
-          'tag_based' => true,
-          'hidden' => true,
-        }
+    # This class generates the GTN's Tag Pages
+    class TagPageGenerator < Generator
+      safe true
 
-        topic_index = PageWithoutAFile.new(site, '', "tags/#{Jekyll::Utils.slugify(tag)}", 'index.md')
-        topic_index.content = ''
-        topic_index.data['layout'] = 'topic'
-        topic_index.data['topic_name'] = "by_tag_#{tag}"
-        topic_index.data['topic'] = site.data["by_tag_#{tag}"]
+      ##
+      # This generates the author pages
+      # Params
+      # +site+:: The site object
+      def generate(site)
+        Jekyll.logger.info '[GTN/SyntheticTopics] Generating By-Tag Indexes'
+        Gtn::TopicFilter.list_all_tags(site).map do |tag|
+          site.data["by_tag_#{tag}"] = {
+            'name' => "by_tag_#{tag}",
+            'type' => 'use',
+            'title' => tag,
+            'summary' => "Tutorials covering #{tag}",
+            'tag_based' => true,
+            'hidden' => true,
+          }
 
-        site.pages << topic_index
-      end
+          topic_index = PageWithoutAFile.new(site, '', "tags/#{Jekyll::Utils.slugify(tag)}", 'index.md')
+          topic_index.content = ''
+          topic_index.data['layout'] = 'topic'
+          topic_index.data['topic_name'] = "by_tag_#{tag}"
+          topic_index.data['topic'] = site.data["by_tag_#{tag}"]
 
-      Jekyll.logger.info '[GTN/SyntheticTopics] Generating By-Tag Embeds'
-      Gtn::TopicFilter.list_all_tags(site).map do |tag|
-        topic_index = PageWithoutAFile.new(site, '', "tags/#{Jekyll::Utils.slugify(tag)}", 'embed.html')
-        topic_index.content = ''
-        topic_index.data['layout'] = 'topic-embed'
-        topic_index.data['topic_name'] = "by_tag_#{tag}"
-        topic_index.data['topic'] = site.data["by_tag_#{tag}"]
+          site.pages << topic_index
+        end
 
-        site.pages << topic_index
+        Jekyll.logger.info '[GTN/SyntheticTopics] Generating By-Tag Embeds'
+        Gtn::TopicFilter.list_all_tags(site).map do |tag|
+          topic_index = PageWithoutAFile.new(site, '', "tags/#{Jekyll::Utils.slugify(tag)}", 'embed.html')
+          topic_index.content = ''
+          topic_index.data['layout'] = 'topic-embed'
+          topic_index.data['topic_name'] = "by_tag_#{tag}"
+          topic_index.data['topic'] = site.data["by_tag_#{tag}"]
+
+          site.pages << topic_index
+        end
       end
     end
   end
--- _plugins/generator-workflows.rb
+++ _plugins/generator-workflows.rb
@@ -4,54 +4,63 @@ require './_plugins/gtn'
 
 module Jekyll
   ##
-  # This class generates the GTN's author pags
-  class WorkflowPageGenerator < Generator
-    safe true
+  # Generators are Jekyll's way to let you generate files at runtime, without needing them to exist on disk.
+  #
+  # We use generators for lots of purposes, e.g.
+  #
+  # +Jekyll::Generators::WorkflowPageGenerator+ emits a webpage for every workflow in the GTN
+  # +Jekyll::Generators::AuthorPageGenerator+ emits a hall-of-fame entry for every contributor, organisation, and grant listed in our site metadata.
+  module Generators
 
     ##
-    # This generates the recording pages, where needed.
-    # Params
-    # +site+:: The site object
-    def generate(site)
-      Jekyll.logger.info "[GTN/Workflows] Generating workflow pages"
-      materials = Gtn::TopicFilter
-        .list_all_materials(site)
+    # This class generates the GTN's workflow pages.
+    class WorkflowPageGenerator < Generator
+      safe true
 
-      # [{"workflow"=>"Calling_variants_in_non-diploid_systems.ga",
-      #   "tests"=>true,
-      #   "url"=>"https://training.galaxyproject.org/training-material/topics/variant-analysis/tutorials/non-dip/workflows/Calling_variants_in_non-diploid_systems.ga",
-      #   "path"=>"topics/variant-analysis/tutorials/non-dip/workflows/Calling_variants_in_non-diploid_systems.ga",
-      #   "wfid"=>"variant-analysis-non-dip",
-      #   "wfname"=>"calling-variants-in-non-diploid-systems",
-      #   "trs_endpoint"=>"https://training.galaxyproject.org/training-material/api/ga4gh/trs/v2/tools/variant-analysis-non-dip/versions/calling-variants-in-non-diploid-systems",
-      #   "license"=>nil,
-      #   "parent_id"=>"variant-analysis/non-dip",
-      #   "topic_id"=>"variant-analysis",
-      #   "tutorial_id"=>"non-dip",
-      #   "creators"=>[],
-      #   "name"=>"Calling variants in non-diploid systems",
-      #   "title"=>"Calling variants in non-diploid systems",
-      #   "test_results"=>nil,
-      #   "modified"=>2024-03-18 12:38:46.293831071 +0100,
-      #   "mermaid"=>
-      #   "graph_dot"=>
-      # }]
-      # /api/workflows/#{topic_id}/#{tutorial_id}/#{wfid}/rocrate.zip
-      shortlinks = site.data['shortlinks']['id'].invert
+      ##
+      # Params
+      # +site+:: The site object
+      def generate(site)
+        Jekyll.logger.info "[GTN/Workflows] Generating workflow pages"
+        materials = Gtn::TopicFilter
+          .list_all_materials(site)
 
-      materials.each do |material|
-        (material['workflows'] || []).each do |workflow|
-          page2 = PageWithoutAFile.new(site, '', '', "#{workflow['path'].gsub(/.ga$/, '.html')}")
-          path = File.join('/', workflow['path'].gsub(/.ga$/, '.html'))
-          page2.content = nil
-          page2.data['title'] = workflow['title']
-          page2.data['layout'] = 'workflow'
-          page2.data['material'] = material
-          page2.data['workflow'] = workflow
-          page2.data['js_requirements'] = {'mathjax' => false, 'mermaid' => true}
-          page2.data['short_id'] = shortlinks[path]
-          page2.data['redirect_from'] = ["/short/#{shortlinks[path]}"]
-          site.pages << page2
+        # [{"workflow"=>"Calling_variants_in_non-diploid_systems.ga",
+        #   "tests"=>true,
+        #   "url"=>"https://training.galaxyproject.org/training-material/topics/variant-analysis/tutorials/non-dip/workflows/Calling_variants_in_non-diploid_systems.ga",
+        #   "path"=>"topics/variant-analysis/tutorials/non-dip/workflows/Calling_variants_in_non-diploid_systems.ga",
+        #   "wfid"=>"variant-analysis-non-dip",
+        #   "wfname"=>"calling-variants-in-non-diploid-systems",
+        #   "trs_endpoint"=>"https://training.galaxyproject.org/training-material/api/ga4gh/trs/v2/tools/variant-analysis-non-dip/versions/calling-variants-in-non-diploid-systems",
+        #   "license"=>nil,
+        #   "parent_id"=>"variant-analysis/non-dip",
+        #   "topic_id"=>"variant-analysis",
+        #   "tutorial_id"=>"non-dip",
+        #   "creators"=>[],
+        #   "name"=>"Calling variants in non-diploid systems",
+        #   "title"=>"Calling variants in non-diploid systems",
+        #   "test_results"=>nil,
+        #   "modified"=>2024-03-18 12:38:46.293831071 +0100,
+        #   "mermaid"=>
+        #   "graph_dot"=>
+        # }]
+        # /api/workflows/#{topic_id}/#{tutorial_id}/#{wfid}/rocrate.zip
+        shortlinks = site.data['shortlinks']['id'].invert
+
+        materials.each do |material|
+          (material['workflows'] || []).each do |workflow|
+            page2 = PageWithoutAFile.new(site, '', '', "#{workflow['path'].gsub(/.ga$/, '.html')}")
+            path = File.join('/', workflow['path'].gsub(/.ga$/, '.html'))
+            page2.content = nil
+            page2.data['title'] = workflow['title']
+            page2.data['layout'] = 'workflow'
+            page2.data['material'] = material
+            page2.data['workflow'] = workflow
+            page2.data['js_requirements'] = {'mathjax' => false, 'mermaid' => true}
+            page2.data['short_id'] = shortlinks[path]
+            page2.data['redirect_from'] = ["/short/#{shortlinks[path]}"]
+            site.pages << page2
+          end
         end
       end
     end
