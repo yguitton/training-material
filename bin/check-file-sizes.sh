70d844b1a212949187bfb10285aee5b5c61e59a3
Saskia Hiltemann <saskia.hiltemann@protonmail.com>
2025-04-30 17:32:13 +0200

    add some hints on reducing file size

--- bin/check-file-sizes.sh
+++ bin/check-file-sizes.sh
@@ -7,6 +7,9 @@ exitcode=0
 for TEST_DATA in topics/*/tutorials/*/workflows/test-data; do
 
 if [[ $(du -sb $TEST_DATA | cut -f1) -gt 10485760 ]]; then
+  if [[ $exitcode -eq 0 ]]; then
+    echo "ERROR: test-data folder may not exceed 10MB. To reduce size, please use zenodo links for input files (and replace 'path:' with 'location:'). Replace output files with asserts"
+  fi
   echo "too big: $(du -sh $TEST_DATA)"
   exitcode=1
 fi
