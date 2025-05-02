634c620507c04ff0da40f2cfc8f240aa5b7e9128
Saskia Hiltemann <saskia.hiltemann@protonmail.com>
2025-05-02 13:21:03 +0200

    tweak test output

--- bin/check-file-sizes.sh
+++ bin/check-file-sizes.sh
@@ -8,9 +8,10 @@ for TEST_DATA in topics/*/tutorials/*/workflows/test-data; do
 
 if [[ $(du -sb $TEST_DATA | cut -f1) -gt 10485760 ]]; then
   if [[ $exitcode -eq 0 ]]; then
-    echo "ERROR: test-data folder may not exceed 10MB. To reduce size, please use zenodo links for input files (and replace 'path:' with 'location:'). Replace output files with asserts"
+    echo "ERROR: test-data folder may not exceed 10MB."
+    echo "  To reduce size, please use zenodo links for files (and replace 'path:' with 'location:'). And consider replacing output files with asserts"
   fi
-  echo "too big: $(du -sh $TEST_DATA)"
+  echo "  Folder too big: $(du -sh $TEST_DATA)"
   exitcode=1
 fi
 
